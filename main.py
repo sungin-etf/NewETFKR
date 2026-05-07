ae31c5a583c9d81a8eb1e4dee2f5a7af4f8e96a6
sungin-etf <sungin.etf@gmail.com>
2026-05-07 13:05:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from KRXCode import check_new_etf_KRXCode
+from DART import check_new_etf_DART
 
 if __name__ == "__main__":
     print("=== KRXCode 실행 ===")
     check_new_etf_KRXCode()
+
+    print("=== DART 실행 ===")
+    check_new_etf_DART()
+
     print("=== 완료 ===")
